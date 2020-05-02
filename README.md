a374dda2728fb12ffd1b5d93728094898ce8625f
Vedant Purohit <vdpurohit13@gmail.com>
2020-05-02 12:25:38 -0400

    Added ship

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Simple game where user operates a rocket ship that shoots down alien invaders.
 
 Game made as a practice project to get more familiar with python. Game is part of
-the book, (Python Crash Course)[https://nostarch.com/pythoncrashcourse2e]
+the book, [Python Crash Course](https://nostarch.com/pythoncrashcourse2e)
--- alien_invasion.py
+++ alien_invasion.py
@@ -2,15 +2,22 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
   """Overall class to manage game assets and behavior."""
 
   def __init__(self):
     """Initialize the game, and create game resources."""
     pygame.init()
+    self.settings = Settings()
 
-    self.screen = pygame.display.set_mode((1200, 800))
+    self.screen = pygame.display.set_mode(
+      (self.settings.screen_width, self.settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    self.ship = Ship(self)
 
   def run_game(self):
     """Start the main loop for the game."""
@@ -20,6 +27,10 @@ class AlienInvasion:
         if event.type == pygame.QUIT:
           sys.exit()
       
+      # Redraw the screen during each pass through the loop.
+      self.screen.fill(self.settings.bg_color)
+      self.ship.blitme()
+
       # Make the most recently drawn screen visible.
       pygame.display.flip()
 
