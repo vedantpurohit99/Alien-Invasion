7d3143aa03fa533232d0834e83144f32b2f64b42
Vedant Purohit <vdpurohit13@gmail.com>
2020-05-03 16:36:27 -0400

    Added alien-bullet collisions

--- alien_invasion.py
+++ alien_invasion.py
@@ -126,6 +126,18 @@ class AlienInvasion:
     for bullet in self.bullets.copy():
       if bullet.rect.bottom <= 0:
         self.bullets.remove(bullet)
+    
+    self._check_bullet_alien_collisions()
+
+  def _check_bullet_alien_collisions(self):
+    """Respond to bullet-alien collisions."""
+    # Remove any bullets and aliens that have collided.
+    collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+    if not self.aliens:
+      # Destroy existing bullets and create new fleet.
+      self.bullets.empty()
+      self._create_fleet()
 
   def _update_screen(self):
     """Update images on the screen, and flip to the new screen."""
--- settings.py
+++ settings.py
@@ -9,10 +9,10 @@ class Settings:
     self.bg_color = (230, 230, 230)
 
     # Ship Settings
-    self.ship_speed = 1.5
+    self.ship_speed = 6
 
     # Bullet settings
-    self.bullet_speed = 1.0
+    self.bullet_speed = 6
     self.bullet_width = 3
     self.bullet_height = 15
     self.bullet_color = (60, 60, 60)
