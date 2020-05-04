8147c489a4f076bc135165ca71abe96a338e6a83
Vedant Purohit <vdpurohit13@gmail.com>
2020-05-04 11:52:10 -0400

    Added dynamic speeds

--- alien_invasion.py
+++ alien_invasion.py
@@ -133,6 +133,8 @@ class AlienInvasion:
     """Start a new game when player clicks Play."""
     button_clicked = self.play_button.rect.collidepoint(mouse_pos)
     if button_clicked and not self.stats.game_active:
+      # Reset game settings.
+      self.settings.initialize_dynamic_settings()
       # Reset the game statistics.
       self.stats.reset_stats()
       self.stats.game_active = True
@@ -208,6 +210,7 @@ class AlienInvasion:
       # Destroy existing bullets and create new fleet.
       self.bullets.empty()
       self._create_fleet()
+      self.settings.increase_speed()
 
   def _update_screen(self):
     """Update images on the screen, and flip to the new screen."""
